42b78e4095844cb231cddb80880733cb97387be9
gaspy2015 <rdvincentgaspar@gmail.com>
2023-09-09 16:07:23 +0800

    signin-signup
    created the ejs file for login and signup

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+require('dotenv').config();
+const ejs = require('ejs');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use('/static', express.static(path.join(__dirname, 'public')));
+app.use('/assets', express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'src')));
+app.use(express.static(path.join(__dirname)));
+
+app.get('/login', (req, res) => {
+    res.render('login')
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`The server is listening at http://${process.env.HOSTNAME}:${process.env.PORT}`);
+});
