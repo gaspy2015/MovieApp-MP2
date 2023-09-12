2e81592f9709ac3c09b4449dd0460a04f257a450
FranChiz28 <fcssalvador28@gmail.com>
2023-09-12 19:17:38 +0800

    Francis
    >drafted dashboard
    >drafted movie info
    >login page returned to sole html since bootstrap is overlapping with tailwind

--- app.js
+++ app.js
@@ -16,11 +16,21 @@ app.use('/assets', express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'src')));
 app.use(express.static(path.join(__dirname)));
 
+app.get('/', (req, res) => {
+    res.render('login', {title: 'Log In'});
+});
+
 app.get('/login', (req, res) => {
     res.render('login', {title: 'Log In'});
 });
 
+app.get('/dashboard', (req, res) => {
+    res.render('dashboard', {title:'Dashboard', user:'Name', userRevenue: 'Php 10,000.00', allRevenue:'Php 100,000.00'}); //change revenue to var
+});
 
+app.get('/movieInfo', (req, res) => {
+    res.render('movieInfo', {title:'Movie Info'});
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`The server is listening at http://${process.env.HOSTNAME}:${process.env.PORT}`);
